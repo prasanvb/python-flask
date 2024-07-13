e0ef689d4cc344e0afd2ef5b92d7e607be41b722
prasanv <prasan.venkatb@gmail.com>
2024-07-12 22:57:13 -0700

    added app.py

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+stores = [{"name": "ikea", "items": [{"name": "lamp", "price": 15.99}]}]
+
+
+@app.get("/stores")  # GET:http://127.0.0.1:5000/stores
+def get_stores():
+    return {"stores": stores}
+
+
+@app.post("/store")  # POST:http://127.0.0.1:5000/store # body:{"store":"home depot"}
+def create_store():
+    request_json_data = request.get_json()
+    print({"type": type(request_json_data).__name__, "data": request_json_data})
+    new_store = {"name": request_json_data["store"], "items": []}
+    stores.append(new_store)
+    return stores, 201
+
+
+@app.post(
+    "/store/<string:name>/item"
+)  # POST:http://127.0.0.1:5000/store/ikea/item  # body:{'item': 'table', 'price': 72.0}}
+def create_store_item(name):
+    request_json_data = request.get_json()
+    for store in stores:
+        if store["name"] == name:
+            store["items"].append(request_json_data)
+            return store, 201
+    return {"error_message": "store not found"}, 404
+
