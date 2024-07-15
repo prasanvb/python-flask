215e3e01afe689384e3cc5255687dd65f8d9989d
prasanv <34255378+prasanvb@users.noreply.github.com>
2024-07-15 22:14:54 +0000

    minor updates

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
 from flask import Flask, request
 from db import stores, items
+
 app = Flask(__name__)
 
 stores = [{"name": "ikea", "items": [{"name": "lamp", "price": 15.99}]}]
 
 
-@app.get("/stores")  # GET:http://127.0.0.1:5000/stores
-def get_stores():
-
-
-@app.get("/")  # GET:http://127.0.0.1:5000/
+# GET:http://127.0.0.1:5000/
+@app.get("/")
 def main():
     return "Python flask project", 200
 
+
+# GET:http://127.0.0.1:5000/stores
+@app.get("/stores")
+def get_stores():
     return {"stores": stores}, 200
 
 
@@ -26,9 +28,10 @@ def create_store():
     return stores, 201
 
 
+# POST:http://127.0.0.1:5000/store/ikea/item  # body:{'item': 'table', 'price': 72.0}}
 @app.post(
     "/store/<string:name>/item"
-)  # POST:http://127.0.0.1:5000/store/ikea/item  # body:{'item': 'table', 'price': 72.0}}
+)
 def create_store_item(name):
     request_json_data = request.get_json()
     for store in stores:
